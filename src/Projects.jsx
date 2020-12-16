98a45194c226a4f5ecba0b09dc9acc67dddabb2a
unknown <12siricoa94@gmail.com>
2020-12-16 00:02:48 -0500

    added images for projects page

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -11,6 +11,9 @@ export default function Project () {
                     <h1 className="headTitle">Projects</h1>
                     <div className="projectContainer">
                         <h2>React Quiz</h2>
+                        <div className='projectImgDiv'>
+                            <div id='reactQuizImgDiv'></div>
+                        </div>
                         <h5>This application is built on React.Js and is intended to test your knowledge on it as well! Once you open the app, you will be presented with a button. Upon pressing the button, you will have activated the quiz. Each question allows ten seconds to answer so think fast and have fun!</h5>
                         <h5>Build</h5>
                         <ul>
@@ -25,6 +28,9 @@ export default function Project () {
                     </div>
                     <div className="projectContainer">
                         <h2>Dragonball Hangman</h2>
+                        <div className='projectImgDiv'>
+                            <div id='dbHangmanImgDiv'></div>
+                        </div>
                         <h5>Below is a game I created called Dragonball hangman! This is one of my earlier projects, and although quite simple, it encapsulates the classic hangman experience. Even the most seasoned Dragonball fan will find difficulty in guessing the correct name, but no worries as you can always keep playing just by pressing another key!</h5>
                         <h5>Build</h5>
                         <ul>
@@ -37,6 +43,9 @@ export default function Project () {
                     </div>
                     <div className="projectContainer">
                         <h2>Hominid Trivia Game</h2>
+                        <div className='projectImgDiv'>
+                            <div id='hominidImgDiv'></div>
+                        </div>
                         <h5>Hominid Trivia Game is a mobile friendly application that tests your knowledge on the origins of the human species. Although simple in scope, this application is easy on the eyes as well as entertaining.</h5>
                         <h5>Build</h5>
                         <ul>
@@ -49,6 +58,9 @@ export default function Project () {
                     </div>
                     <div className="projectContainer">
                         <h2>Burgers!</h2>
+                        <div className='projectImgDiv'>
+                            <div id='burgersImgDiv'></div>
+                        </div>
                         <h5>This is an app designed for the user to create a burger. They can enter a name, and also set the state of the burger to either Fresh or Devoured! After the burger is made, the user can devour the burger, or make it fresh, and watch it switch sides.</h5>
                         <h5>Build</h5>
                         <ul>
@@ -65,6 +77,9 @@ export default function Project () {
                     </div>
                     <div className="projectContainer">
                         <h2>Found Sound</h2>
+                        <div className='projectImgDiv'>
+                            <div id='foundSoundImgDiv'></div>
+                        </div>
                         <h5>Found Sound is the perfrect application for anyone who would like to sample some music by an artist. Simply type into the search bar an artist, song, or album name and Found Sound will return you a list of samples based off the search phrase that was provided. Having a simple, yet elegant design, sound found is easy to navigate and use for any user.</h5>
                         <h5>Build</h5>
                         <ul>
@@ -78,6 +93,9 @@ export default function Project () {
                     </div>
                     <div className="projectContainer">
                         <h2>Rico Depot</h2>
+                        <div className='projectImgDiv'>
+                            <div id='ricoDepotImgDiv'></div>
+                        </div>
                         <h5>Rico Depot is a marketplace/game application that works as follows. Upon signing up a user is given 10,000 credits and is allowed to purchase products listed on the products page. When a user buys a product, their purchase history will be appened to their homepage. Users can also view how their spending habits score up against other users on the leaderboard page</h5>
                         <h5>Build</h5>
                         <ul>
